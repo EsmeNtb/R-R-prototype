1ce51ffd0b6b5eef0927c11638a5adc9d74ff917
Kenia Esmeralda Ramos Javier <esme.E_N.tb@outlook.com>
2026-05-14 14:53:34 -0600

    Create Home.blade.php

---
+++ prototype/resources/views/Home.blade.php
@@ -0,0 +1,16 @@
+<html lang = "en">
+    <head>
+        <meta charset = "utf-8">
+        <meta name = "viewport" content= "width=device-width" , initial-scale=1>
+        
+        <title>Prototype Page</title>
+    </head>
+    <body>
+        <head>
+        </head>
+        <!--<div>
+            -- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh 
+        </div> -->
+
+    </body>
+</html>
