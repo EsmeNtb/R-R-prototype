c96fc34bf42f0216159bc0ee8cd1e8d9bc0989f4
Kenia Esmeralda Ramos Javier <esme.E_N.tb@outlook.com>
2026-05-15 13:07:55 -0600

    feat: add vite

--- prototype/resources/views/Home.blade.php
+++ prototype/resources/views/Home.blade.php
@@ -2,13 +2,17 @@
     <head>
         <meta charset = "utf-8">
         <meta name = "viewport" content= "width=device-width" , initial-scale=1>
-        
+        @vite('resources/css/app.css')
         <title>Prototype Page</title>
     </head>
     <body>
+    <h1 class= "text-3x1 front-bold underline">
+        En progreso
+    </h1>
+        <!--
         <head>
         </head>
-        <!--<div>
+        <div>
             -- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh 
         </div> -->
 
